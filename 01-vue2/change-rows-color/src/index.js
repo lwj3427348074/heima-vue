662b6956a011a119d3b79e5a6299f581ca7e7196
lwj <3427348074@qq.com>
2022-10-13 19:54:58 +0800

    P6

---
+++ 01-vue2/change-rows-color/src/index.js
@@ -0,0 +1,10 @@
+//1.使用ES6导入语法,导入jQuery
+import $ from 'jquery'
+
+
+//2.定义jQuery的入口函数
+$(function () {
+    //3.实现奇偶行的变色效果
+    $('li:odd').css('background-color', 'red')
+    $('li:even').css('background-color', 'pink')
+})
