67441c4b9a17dacf858efd8dbfb254d35f37e4f2
lwj <3427348074@qq.com>
2022-10-24 19:58:19 +0800

    路由的基本用法

---
+++ 06-vue2/router-demo1/src/App2.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-container">
+    <h1>App2 组件</h1>
+
+    
+    <!-- <a href="#/home">首页</a>
+    <a href="#/movie">电影</a>
+    <a href="#/about">关于</a> -->
+
+    <!-- 只要在项目中安装并配置了vue-router,就可以使用router-link来替代普通的a链接了 -->
+    <router-link to="/home">首页</router-link>
+    <router-link to="/movie">电影</router-link>
+    <router-link to="/about">关于</router-link>
+    
+    <hr />
+
+    <!-- 只要在项目中安装并配置了vue-router,就可以使用router-view这个组件了 -->
+    <!-- 它的作用很单纯,占位符 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less" scoped>
+.app-container {
+  background-color: #efefef;
+  overflow: hidden;
+  margin: 10px;
+  padding: 15px;
+  > a {
+    margin-right: 10px;
+  }
+}
+</style>
---
+++ 06-vue2/router-demo1/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App2.vue'
+//导入路由模块,拿到路由的实例对象
+//在进行模块化导入的时候,如果给定的是文件夹,则默认导入这个文件夹下index.js的文件
+import router from '@/router'
+
+// 导入 bootstrap 样式
+import 'bootstrap/dist/css/bootstrap.min.css'
+// 全局样式
+import '@/assets/global.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  // 在vue项目中,要想把路由用起来,必须把路由实例对象,通过下面的方式进行挂载
+  // router:路由的实例对象
+  // router:router简写=>router
+  router
+}).$mount('#app')
